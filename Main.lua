a0c0957b5a5178f5c367c09f96b131f5484ebfba
kolya112 <59023595+kolya112@users.noreply.github.com>
2021-10-30 16:03:34 +0300

    1.08
    Localization has been added. Now the program available is in English!

--- Main.lua
+++ Main.lua
@@ -20,6 +20,7 @@ local maxenergy = math.modf(computer.maxEnergy())
 local uptime = math.modf(computer.uptime())
 local efiname = EFI.getLabel()
 local boot = EFI.getData()
+local apath = fs.path(system.getCurrentScript())
 
 ---------------------------------------------------------------------------------
 
@@ -27,47 +28,60 @@ local boot = EFI.getData()
 local workspace, window, menu = system.addWindow(GUI.filledWindow(1, 1, 113, 43, 0xE1E1E1))
 
 -- Get localization table dependent of current system language
+local lang = system.getCurrentScriptLocalization()
 
 -- Add single cell layout to window
 local layout = window:addChild(GUI.layout(1, 1, window.width, window.height, 1, 1))
 
--- Функции
+-- Functions
 
 local function addButton(text)
-return layout:addChild(GUI.roundedButton(1, 1, 36, 3, 0xD2D2D2, 0x696969, 0x4B4B4B, 0xF0F0F0, text))
+  return layout:addChild(GUI.roundedButton(1, 1, 36, 3, 0xD2D2D2, 0x696969, 0x4B4B4B, 0xF0F0F0, text))
+end
+
+local function draw()
+  layout:addChild(GUI.image(1, 1, image.load(apath .. "/Icon.pic")))
 end
 
 local function addText(text)
-layout:addChild(GUI.text(1, 1, 0x696969, text))
+  layout:addChild(GUI.text(1, 1, 0x696969, text))
 end
 
--- Add nice gray text object to layout
-layout:addChild(GUI.image(1, 1, image.load(cache .. "/Icon.pic")))
-layout:addChild(GUI.text(2, 2, 0x4B4B4B, "Привет, ".. system.getUser()))
-layout:addChild(GUI.text(3, 3, 0x4B4B4B, totalMemoryKB .." КБ (".. totalMemoryMB .. " МБ) оперативной памяти у Вас всего"))
-layout:addChild(GUI.text(4, 4, 0x4B4B4B, freeMemoryKB .. " КБ (".. freeMemoryMB .. " МБ) оперативной памяти у Вас свободно"))
-layout:addChild(GUI.text(5, 5, 0x4B4B4B, lvlenergy .. " зафиксированная энергия на компьютере(".. maxenergy .." максимальная энергия)"))
-layout:addChild(GUI.text(6, 6, 0x4B4B4B, uptime .. " секунд работает компьютер"))
-layout:addChild(GUI.text(7, 7, 0x4B4B4B, efiname .. " - название EFI"))
-layout:addChild(GUI.text(8, 8, 0x4B4B4B, boot .. " - адрес диска, с которого запущена операционная система"))
-layout:addChild(GUI.text(10, 10, 0x4B4B4B, address .." - адрес устройства"))
-if computer.users() == "" then
-layout:addChild(GUI.text(11, 11, 0x4B4B4B, "Список пользователей устройства:".. computer.users()))
-end
-local lable = layout:addChild(GUI.input(15, 15, 30, 3, 0xEEEEEE, 0x555555, 0x999999, 0xFFFFFF, 0x2D2D2D, "", "Введите новое название EFI"))
-addButton("Изменить название EFI").onTouch = function()
-  if #lable.text > 0 then
-    EFI.setLabel(lable.text)
-    GUI.alert("Новое название EFI успешно установлено!")
-  else
-    GUI.alert("Пустая строка.")
+local function addTab(text, func)
+  window.tabBar:addItem(text).onTouch = function()
+    layout:removeChildren()
+    draw()
+    func()
+    workspace:draw()
   end
 end
-layout:addChild(GUI.text(16, 16, 0x33DB40, "Версия 1.07 stable"))
-layout:addChild(GUI.text(17, 17, 0x0049FF, "Разработано на проекте Hilarious(hil.su)"))
+-- Add nice gray text object to layout
+layout:addChild(GUI.image(1, 1, image.load(cache .. "/Icon.pic")))
+layout:addChild(GUI.text(2, 2, 0x4B4B4B, lang.welcome .. system.getUser()))
+layout:addChild(GUI.text(3, 3, 0x4B4B4B, totalMemoryKB .. lang.KB .. totalMemoryMB .. lang.MBtotal))
+layout:addChild(GUI.text(4, 4, 0x4B4B4B, freeMemoryKB .. lang.KB .. freeMemoryMB .. lang.MBfree))
+layout:addChild(GUI.text(5, 5, 0x4B4B4B, lvlenergy .. lang.lvlenergy.. maxenergy .. lang.maxenergy))
+layout:addChild(GUI.text(6, 6, 0x4B4B4B, uptime .. lang.uptime))
+layout:addChild(GUI.text(7, 7, 0x4B4B4B, efiname .. lang.efiname))
+layout:addChild(GUI.text(8, 8, 0x4B4B4B, boot .. lang.boot))
+layout:addChild(GUI.text(10, 10, 0x4B4B4B, address .. lang.address))
+if computer.users() == "" then
+layout:addChild(GUI.text(11, 11, 0x4B4B4B, lang.userslist .. computer.users()))
+end
+local lable = layout:addChild(GUI.input(15, 15, 30, 3, 0xEEEEEE, 0x555555, 0x999999, 0xFFFFFF, 0x2D2D2D, "", lang.efinameinput))
+addButton(lang.editefiname).onTouch = function()
+  if #lable.text > 0 then
+    EFI.setLabel(lable.text)
+    GUI.alert(lang.efiname)
+  else
+    GUI.alert(lang.emptystring)
+  end
+end
+layout:addChild(GUI.text(16, 16, 0x33DB40, lang.version))
+layout:addChild(GUI.text(17, 17, 0x0049FF, lang.dev))
 
 -- You can also add items without context menu
-menu:addItem("Выйти").onTouch = function()
+menu:addItem(lang.exit).onTouch = function()
   window:remove()
 end
 
